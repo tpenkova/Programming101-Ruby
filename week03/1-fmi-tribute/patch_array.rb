8a00dee7c8dcb85b6659a03715fd775029fc07dc
Tanya <tanya@penkova.net>
2016-04-13 01:28:45 +0300

    Add solutions

--- week03/1-fmi-tribute/patch_array.rb
+++ week03/1-fmi-tribute/patch_array.rb
@@ -1,3 +1,31 @@
 class Array
-  # Your code goes here.
+  def to_hash
+	hash = {}
+	each do |element|
+		hash[element[0]] = element[1]
+	end
+
+	hash
+  end
+
+  def index_by
+	hash = {}
+	each do |element|
+		hash[yield(element)] = element
+	end
+
+	hash
+  end
+
+  def subarray_count array
+	count = 0 
+	each_cons(array.length) {|n| count +=1 if n == array } 
+	count
+  end
+
+  def occurences_count
+	hash = {}
+	each {|n| hash[n] = self.count(n) }
+	hash
+  end
 end
